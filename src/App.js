d33606a3be2e6cc20daaf95bfc1ca560a70280f2
PA4KEV <kevin.vd.vleuten@gmail.com>
2022-12-29 15:53:24 +0100

    Add basic navigation and content item components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+import './App.css';
+import Navigation from './UI/Navigation';
+import ContentItem from './Content/ContentItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <button type="button" class="btn btn-primary">Primary</button>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div>
+      <Navigation />
+
+      <ContentItem />
+      <ContentItem />
+      <ContentItem />
+
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/Content/ContentItem.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const ContentItem = (props) => {
+    return (
+        <div>
+            <p>
+            Whether we like it or not, we have all been born on this earth as part of one great human family. Rich or poor, educated or uneducated, belonging to one nation or another, to one religion or another, adhering to this ideology or that, ultimately each of us is just a human being like everyone else: we all desire happiness and do not want suffering. Furthermore, each of us has an equal right to pursue these goals.
+            </p>
+            <p>
+            Let us cultivate love and compassion, both of which give life true meaning. This is the religion I preach. It is simple. Its temple is the heart. Its teaching is love and compassion. Its moral values are loving and respecting others, whoever they may be. Whether one is a lay person or a monastic, we have no other option if we wish to survive in this world.
+            </p>
+        </div>
+    )
+}
+
+export default ContentItem;
