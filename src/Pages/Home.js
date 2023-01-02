2deba816f34235b45ea63d86d98d9de884137b97
PA4KEV <kevin.vd.vleuten@gmail.com>
2023-01-02 21:46:47 +0100

    Update home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,34 +2,32 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 const Home = () => {
-    return (        
+    return (
         <div className='row'>
             <div className="col-6 offset-3">
                 <section className="content-section">  
-                <h1>Kevin van der Vleuten</h1>
-                <p>Welcome to my simple website.</p>
-
-                <p>I am a software developer and enjoy creating solutions.                    
-                </p>
-
-                <p>Check out my <Link to="/pages/software">Software</Link> journey.<br/> I also enjoy working with <Link to="/pages/linux">Linux</Link>.</p>
-
-                <p>
-                    <a href="https://github.com/PA4KEV" target="_blank" rel="noreferrer">
-                        <img src={`${process.env.PUBLIC_URL}/icons/github.png`} alt="github icon"/>
-                    </a>                    
-                </p>
-                
-                <hr/>
-
-                <p>Aside from software tinkering, check out my other interests here:</p>
-                <ul>
-                    <li><Link to="/pages/radio">Radio amateur</Link></li>
-                </ul>
-
-                <hr/>
-                <p class="small">This site was made with: <a href="https://reactjs.org/" target="_blank">React</a>, <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a>, <a href="https://www.nginx.com/" target="_blank">Nginx</a> and <a href="https://www.docker.com/" target="_blank">Docker</a><br/>on <a href="https://www.digitalocean.com/" target="_blank">DigitalOcean</a> cloud droplets.</p>
-
+                    <h1>Hallo, my name is Kevin.</h1>
+                    <h3>Nice to meet you! :)</h3>
+                    <p>I have done <Link to="/pages/software">configuration automation</Link>, <Link to="/pages/software/dxp-development">DXP development</Link> and ...</p>
+                    <p>When it comes to technology and engineering, I am always curious and eager to learn and adept myself. One of the things I enjoy the most is being able to teach others. Interns who enthousiastically become better programmers themselves through my knowledge is amazing! Check out my <Link to="/pages/software">Software</Link> journey.</p>
+                    <p>My other interests are:</p>
+                    <ul>
+                        <li><Link to="/pages/radio">amateur radio</Link></li>
+                        <li><Link to="/pages/linux">Linux</Link></li>
+                        <li>electronics</li>
+                        <li>learning Japanese</li>
+                        <li>woodworking</li>
+                        <li>synthesize music</li>
+                        <li>yoga</li>
+                    </ul>
+                    <p>I am based in Helmond, the Netherlands.</p>
+                    <p>
+                        <a href="https://github.com/PA4KEV" target="_blank" rel="noreferrer">
+                            <img src={`${process.env.PUBLIC_URL}/icons/github.png`} alt="github icon"/>
+                        </a>
+                    </p>
+                    <hr/>
+                    <p class="small">This site was made with: <a href="https://reactjs.org/" target="_blank">React</a>, <a href="https://getbootstrap.com/" target="_blank">Bootstrap</a>, <a href="https://www.nginx.com/" target="_blank">Nginx</a> and <a href="https://www.docker.com/" target="_blank">Docker</a><br/>on <a href="https://www.digitalocean.com/" target="_blank">DigitalOcean</a> cloud droplets.</p>
                 </section>
             </div>
         </div>
