aaa343cb9f8a2f7d526d2ee7de76f3451e216d46
PA4KEV <kevin.vd.vleuten@gmail.com>
2024-02-10 11:34:43 +0100

    Convert fielddays pages from JavaScript to Markdown.

--- src/App.js
+++ src/App.js
@@ -34,11 +34,7 @@ import Components from './Pages/Radio/Components';
 import Balkon from './Pages/Radio/Balkon';
 
 import Fielddays from './Pages/Radio/fielddays/fielddays';
-import FD20231111EN from './Pages/Radio/fielddays/en/20231111'
-import FD20231111NL from './Pages/Radio/fielddays/nl/20231111'
-import FD20231230EN from './Pages/Radio/fielddays/en/20231230'
-import FD20231230NL from './Pages/Radio/fielddays/nl/20231230'
-import fielddaysdata from './Pages/Radio/fielddays/fielddays.json'
+import fielddaysdata from './Pages/Radio/fielddays/fielddays.json';
 
 import MarkdownPage from './Pages/markdownPage';
 
@@ -83,12 +79,12 @@ function App() {
           <Route exact path='/pages/basic' element={<BasicPage id="1" />}></Route>
           <Route exact path='/wine' element={<BasicPage id="4" />}></Route>
 
-          <Route exact path='/en/pages/radio/fielddays/20231111' element={<FD20231111EN />}></Route>
-          <Route exact path='/en/pages/radio/fielddays/20231230' element={<FD20231230EN />}></Route>
+          <Route exact path='/en/pages/radio/fielddays/20231111' element={<MarkdownPage md='Radio/fielddays/en/20231111.md' />}></Route>
+          <Route exact path='/en/pages/radio/fielddays/20231230' element={<MarkdownPage md='Radio/fielddays/en/20231230.md' />}></Route>
           <Route exact path='/en/pages/radio/fielddays/20240121' element={<MarkdownPage md='Radio/fielddays/en/20240121.md' />}></Route>
 
-          <Route exact path='/nl/pages/radio/fielddays/20231111' element={<FD20231111NL />}></Route>
-          <Route exact path='/nl/pages/radio/fielddays/20231230' element={<FD20231230NL />}></Route>
+          <Route exact path='/nl/pages/radio/fielddays/20231111' element={<MarkdownPage md='Radio/fielddays/nl/20231111.md' />}></Route>
+          <Route exact path='/nl/pages/radio/fielddays/20231230' element={<MarkdownPage md='Radio/fielddays/nl/20231230.md' />}></Route>
           <Route exact path='/nl/pages/radio/fielddays/20240121' element={<MarkdownPage md='Radio/fielddays/nl/20240121.md' />}></Route>
 
 
