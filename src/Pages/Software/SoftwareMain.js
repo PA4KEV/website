25e53243196661458e4d2e7387df7402f0640ee1
PA4KEV <kevin.vd.vleuten@gmail.com>
2024-02-23 21:23:02 +0100

    Update software page by parsing json file instead.

--- src/Pages/Software/SoftwareMain.js
+++ src/Pages/Software/SoftwareMain.js
@@ -13,13 +13,32 @@ const SoftwareMain = () => {
             <MarkdownPage md={'Software/' + language + '/main.md'} />
             <hr />
 
+            <h2>{getString('education')}</h2>
+            {educationData.education.map((edu, index) => (
+                <div key={edu.name + index}>
+                    <h4>{edu.name}</h4>
+                    <h6>{edu.diploma}</h6>
+                    <p>{edu.school} - {edu.location}</p>
+                    <p>{edu.start} - {edu.end}</p>
+                </div>
+            ))}
+            <hr />
+            <h2>{getString('certification')}</h2>
+            {educationData.certifications.map((certification, index) => (
+                <div key={certification.name + index}>
+                    <h4>{certification.name}</h4>
+                    <h6><a href={certification.link}>{certification.institution}</a></h6>
+                    <p>{getString('issued')}: {certification.issueYear}</p>
+                </div>
+            ))}
+            <hr />
             <h2>{getString('courses')}</h2>
-            <div class="card-deck">
+            <div className="card-deck">
                 {educationData.courses.map((course, index) => (
-                    <div className='card courses-card' key={index}>
-                        <div class="card-body">
-                            <h5 class="card-title">{course.name}</h5>
-                            <p class="card-text">
+                    <div className='card courses-card' key={course.name + index}>
+                        <div className="card-body">
+                            <h5 className="card-title">{course.name}</h5>
+                            <p className="card-text">
                                 <i className='small'>{course.sub}, {course.instructor}</i>
                                 <br />
                                 <a href={course.link}>{course.intstitution}</a> | <a href={course.certificate}>certificate</a>
@@ -27,8 +46,8 @@ const SoftwareMain = () => {
                                 {getString('completed')}: {course.achieveDate}
                             </p>
                         </div>
-                        <div class="card-footer">
-                            <small class="text-muted">{course.duration} hours</small>
+                        <div className="card-footer">
+                            <small className="text-muted">{course.duration} hours</small>
                         </div>
                     </div>
                 ))}
